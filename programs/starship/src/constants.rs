ae33ed9f1fdb0ba61ae1709b73cb66be6b28bc3f
Tung Nguyen <87560224+tungnguyennhat98@users.noreply.github.com>
2022-02-10 23:11:08 +0700

    rebrand

---
+++ programs/starship/src/constants.rs
@@ -0,0 +1,5 @@
+pub const ROOT_KEYS: &[&str] = &[
+  "FzgJFn3oLjKju356W1LKX4B5yTsfHVKN3yjc6ydgzWaP",
+  "5UrM9csUEDBeBqMZTuuZyHRNhbRW4vQ1MgKJDrKU1U2v",
+  "GnzQDYm2gvwZ8wRVmuwVAeHx5T44ovC735vDgSNhumzQ",
+];
