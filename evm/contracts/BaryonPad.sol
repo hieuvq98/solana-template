ae899138f63735a79e4afd2635f1f1a60fcb7738
Tung Nguyen <87560224+tungnguyennhat98@users.noreply.github.com>
2022-08-18 19:35:39 +0700

    add evm version

---
+++ evm/contracts/BaryonPad.sol
@@ -0,0 +1,370 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./lib/Common.sol";
+import "./lib/Token.sol";
+import "./lib/Module.sol";
+
+/**
+ * @dev These functions deal with verification of Merkle trees (hash trees),
+ */
+library MerkleProof {
+  /**
+    * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
+    * defined by `root`. For this, a `proof` must be provided, containing
+    * sibling hashes on the branch from the leaf to the root of the tree. Each
+    * pair of leaves and each pair of pre-images are assumed to be sorted.
+    */
+  function verify(bytes32[] memory proof, bytes32 root, bytes32 leaf) internal pure returns (bool) {
+    bytes32 computedHash = leaf;
+
+    for (uint256 i = 0; i < proof.length; i++) {
+      bytes32 proofElement = proof[i];
+
+      if (computedHash <= proofElement) {
+        // Hash(current computed hash + current element of the proof)
+        computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
+      } else {
+        // Hash(current element of the proof + current computed hash)
+        computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+      }
+    }
+
+    // Check if the computed hash (root) is equal to the provided root
+    return computedHash == root;
+  }
+}
+
+/**
+ * @dev BaryonPad allows user to create raising fund campaign in a FIFO manner
+ */
+contract BaryonPad is Ownable, Payable {
+
+  mapping(uint256 => mapping(address => uint256)) private _allowances;
+  mapping(uint256 => mapping(address => uint256)) private _exchangeTokenAllowances;
+  mapping(uint256 => LaunchpadData) private _launchpadDatas;
+  mapping(uint256 => mapping(address => ExchangeToken)) _exchangeTokens;
+  mapping(uint256 => mapping(address => bool)) private _registrations;
+
+  using SafeERC20 for IERC20;
+  using SafeMath for uint256;
+
+  /// @dev Initialize a new vault
+  /// @param owner_ Owner of this pad
+  constructor(address owner_) Ownable(owner_) {
+  }
+
+  struct LaunchpadData {
+    address token;
+    uint256 priceN;
+    uint256 priceD;
+    bytes32 privateSaleSignature;
+    uint256 minPerTx;
+    uint256 maxPerUser;
+    uint256 maxAmount;
+    uint256 registerStartTimestamp;
+    uint256 registerEndTimestamp;
+    uint256 redeemStartTimestamp;
+    uint256 redeemEndTimestamp;
+    bool isPrivateSale;
+    bool isActive;
+  }
+
+  struct ExchangeToken {
+    address token;
+    uint256 priceN;
+    uint256 priceD;
+    uint256 maxAmount;
+  }
+
+  event LaunchpadUpdated(uint256 launchpadId);
+  event ExchangeTokenUpdated(uint256 launchpadId, address indexed token);
+  event Registered(uint256 launchpadId, address indexed recipient);
+  event Redeemed(uint256 launchpadId, address indexed recipient, uint256 amount);
+  event Withdrawn(address indexed owner, address indexed recipient, address indexed token, uint256 value);
+
+  /// @dev User enroll to a particular launchpad. User must be registered in order to participate token sale
+  /// @param launchpadId_ Launchpad ID
+  /// @param index_ Ordinal of registration
+  /// @param proofs_ Optional metadata of merkle tree to verify user is eligible to register
+  function register(uint256 launchpadId_, uint256 index_, bytes32[] calldata proofs_) external {
+    LaunchpadData storage launchpad = _launchpadDatas[launchpadId_];
+
+    require(launchpad.isActive, "BaryonPad: Invalid launchpad");
+    require(launchpad.registerStartTimestamp <= block.timestamp && launchpad.registerEndTimestamp >= block.timestamp, "BaryonPad: Not registration time");
+    if(launchpad.isPrivateSale) {
+      bytes32 node = keccak256(abi.encodePacked(index_, _msgSender()));
+      require(MerkleProof.verify(proofs_, launchpad.privateSaleSignature, node), "BaryonPad: Invalid proof");
+    }
+
+    _registrations[launchpadId_][_msgSender()] = true;
+
+    emit Registered(launchpadId_, _msgSender());
+  }
+
+  /// @dev Buy token in the launchpad during redemption time
+  /// @param launchpadId_ Launchpad ID
+  /// @param token_ Type of token user use to exchange
+  /// @param amount_ Amount of {token_} user use to exchange
+  function redeem(uint256 launchpadId_, address token_, uint256 amount_) external payable {
+    LaunchpadData storage launchpad = _launchpadDatas[launchpadId_];
+    require(launchpad.isActive, "BaryonPad: Invalid launchpad");
+    require(launchpad.redeemStartTimestamp <= block.timestamp && launchpad.redeemEndTimestamp >= block.timestamp, "BaryonPad: Not redemption time");
+    require(_registrations[launchpadId_][_msgSender()], "BaryonPad: Not registered");
+
+    require(launchpad.minPerTx == 0 || amount_ >= launchpad.minPerTx, "BaryonPad: Not meet minimum amount");
+    uint256 allowance = _allowances[launchpadId_][_msgSender()];
+    uint256 newAllowance = allowance.add(amount_);
+    require(launchpad.maxPerUser == 0 || newAllowance <= launchpad.maxPerUser, "BaryonPad: Allowance reached");
+
+    if(_msgValue() > 0) {
+      require(launchpad.priceN > 0, "BaryonPad: Native token not supported");
+      require(token_ == address(0), "BaryonPad: Token not supported");
+      uint256 sendingAmount = amount_.mul(launchpad.priceN);
+      sendingAmount = sendingAmount.div(launchpad.priceD);
+      uint256 tokenAllowance = _exchangeTokenAllowances[launchpadId_][token_];
+      uint256 newTokenAllowance = tokenAllowance.add(sendingAmount);
+      require(launchpad.maxAmount == 0 || newTokenAllowance <= launchpad.maxAmount, "BaryonPad: Allowance reached");
+      require(_msgValue() == sendingAmount, "BaryonPad: Insuffient fund");
+    }
+    else {
+      ExchangeToken storage exchangeToken = _exchangeTokens[launchpadId_][token_];
+      require(exchangeToken.priceN > 0, "BaryonPad: Token not supported");
+      uint256 sendingAmount = amount_.mul(exchangeToken.priceN);
+      sendingAmount = sendingAmount.div(exchangeToken.priceD);
+      uint256 tokenAllowance = _exchangeTokenAllowances[launchpadId_][token_];
+      uint256 newTokenAllowance = tokenAllowance.add(sendingAmount);
+      require(exchangeToken.maxAmount == 0 || newTokenAllowance <= exchangeToken.maxAmount, "BaryonPad: Allowance reached");
+      IERC20(exchangeToken.token).safeTransferFrom(_msgSender(), address(this), sendingAmount);
+    }
+
+    _allowances[launchpadId_][_msgSender()] = newAllowance;
+    IERC20(launchpad.token).safeTransfer(_msgSender(), amount_);
+
+    emit Redeemed(launchpadId_, _msgSender(), amount_);
+  }
+
+  /// @dev Create/Update a launchpad. Those parameters can't be changed if the launch passed registration phase
+  /// @param launchpadId_ Launchpad ID
+  /// @param token_ Address of token that will be sold
+  /// @param priceN_ Numerator of `token_` price in native token
+  /// @param priceN_ Denominator of `token_` price in native token
+  /// @param isPrivateSale_ Is this a private sale that need whitelist
+  /// @param privateSaleSignature_ Root of merkle tree to prove a user to eligible to register
+  /// @param minPerTx_ Minimum amount of `token_` must be executed in one transaction. 0 for unlimited
+  /// @param maxPerUser_ Maximum amount of `token_` a user is allowed to buy during the sale. 0 for unlimited
+  /// @param maxAmount_ Maximum amount native token can be used to buy `token_`
+  /// @param timestamps_ Array of timestamps of milestones of the sale
+  ///   0: Registration time start
+  ///   1: Registration time end
+  ///   2: Redemption time start
+  ///   3: Redemption time end
+  /// NOTE: This is a workaround for stack too deep error
+  function setLaunchpad(
+    uint256 launchpadId_, address token_, uint256 priceN_, uint256 priceD_,
+    bool isPrivateSale_, bytes32 privateSaleSignature_, uint256 minPerTx_, uint256 maxPerUser_, uint256 maxAmount_,
+    uint256[] memory timestamps_
+  ) external onlyOwner {
+
+    require(timestamps_.length == 4, "BaryonPad: Invalid arguments");
+
+    require(priceD_ > 0 || priceN_ == 0, "BaryonPad: Invalid price");
+    require(block.timestamp <= timestamps_[0], "BaryonPad: Time must be set in the future");
+    require(timestamps_[0] < timestamps_[1], "BaryonPad: Invalid registration time");
+    require(timestamps_[1] <= timestamps_[2], "BaryonPad: Registration and sale time overlap");
+    require(timestamps_[2] < timestamps_[3], "BaryonPad: Invalid sale time");
+
+    LaunchpadData storage launchpad = _launchpadDatas[launchpadId_];
+
+    if (launchpad.registerStartTimestamp != 0) {
+      require(launchpad.registerStartTimestamp >= block.timestamp, "BaryonPad: Launchpad finalized");
+    }
+
+    launchpad.token = token_;
+    launchpad.priceN = priceN_;
+    launchpad.priceD = priceD_;
+    launchpad.isPrivateSale = isPrivateSale_;
+    launchpad.privateSaleSignature = privateSaleSignature_;
+    launchpad.minPerTx = minPerTx_;
+    launchpad.maxPerUser = maxPerUser_;
+    launchpad.maxAmount = maxAmount_;
+    launchpad.registerStartTimestamp = timestamps_[0];
+    launchpad.registerEndTimestamp = timestamps_[1];
+    launchpad.redeemStartTimestamp = timestamps_[2];
+    launchpad.redeemEndTimestamp = timestamps_[3];
+    launchpad.isActive = true;
+
+    emit LaunchpadUpdated(launchpadId_);
+  }
+
+  /// @dev Get launchpad data
+  /// @param launchpadId_ Launchpad ID
+  function getLaunchpad(uint256 launchpadId_) external view returns (LaunchpadData memory) {
+    return _launchpadDatas[launchpadId_];
+  }
+
+  /// @dev Change launchpad's status
+  /// @param launchpadId_ Launchpad ID
+  /// @param isActive_ Inactive/Active
+  function setLaunchpadStatus(uint256 launchpadId_, bool isActive_) external onlyOwner {
+
+    LaunchpadData storage launchpad = _launchpadDatas[launchpadId_];
+    launchpad.isActive = isActive_;
+
+    emit LaunchpadUpdated(launchpadId_);
+  }
+
+  /// @dev Change launchpad's status
+  /// @param launchpadId_ Launchpad ID
+  /// @param token_ Address of token that will be sold
+  /// @param priceN_ Numerator of price in `token_`
+  /// @param priceN_ Denominator of price in `token_`
+  /// @param maxAmount_ Maximum amount of `token_` can be used to buy launchpad token
+  function setExchangeToken(
+    uint256 launchpadId_, address token_, uint256 priceN_, uint256 priceD_,
+    uint256 maxAmount_
+  ) external onlyOwner {
+    require(token_ != address(0), "BaryonPad: Token is zero address");
+    require(priceD_ > 0 || priceN_ == 0, "BaryonPad: Invalid price");
+
+    ExchangeToken storage exchangeToken = _exchangeTokens[launchpadId_][token_];
+    exchangeToken.token = token_;
+    exchangeToken.priceN = priceN_;
+    exchangeToken.priceD = priceD_;
+    exchangeToken.maxAmount = maxAmount_;
+
+    emit ExchangeTokenUpdated(launchpadId_, token_);
+  }
+
+  /// @dev withdraw the token in the vault, no limit
+  /// @param token_ address of the token, use address(0) to withdraw gas token
+  /// @param destination_ recipient address to receive the fund
+  /// @param amount_ amount of fund to withdaw
+  function withdraw(address token_, address destination_, uint256 amount_) external onlyOwner {
+    require(destination_ != address(0), "BaryonPad: Destination is zero address");
+
+    uint256 availableAmount;
+    if(token_ == address(0)) {
+      availableAmount = address(this).balance;
+    } else {
+      availableAmount = IERC20(token_).balanceOf(address(this));
+    }
+
+    require(amount_ <= availableAmount, "BaryonPad: Not enough balance");
+    if(token_ == address(0)) {
+      destination_.call{value:amount_}("");
+    } else {
+      IERC20(token_).safeTransfer(destination_, amount_);
+    }
+
+    emit Withdrawn(_msgSender(), destination_, token_, amount_);
+  }
+
+  /// @dev withdraw NFT from contract
+  /// @param token_ address of the token, use address(0) to withdraw gas token
+  /// @param destination_ recipient address to receive the fund
+  /// @param tokenId_ ID of NFT to withdraw
+  function withdrawNft(address token_, address destination_, uint256 tokenId_) external onlyOwner {
+    require(destination_ != address(0), "BaryonPad: destination is zero address");
+
+    IERC721(token_).transferFrom(address(this), destination_, tokenId_);
+
+    emit Withdrawn(_msgSender(), destination_, token_, 1);
+  }
+}
+
+contract BaryonPadFactory is Ownable, Payable {
+
+  using SafeERC20 for IERC20;
+
+  constructor () Ownable(_msgSender()) {
+  }
+
+  /// @dev Emit `Created` when a new vault is created
+  event Created(address indexed vault);
+  /// @dev Emit `Withdrawn` when owner withdraw fund from the factory
+  event Withdrawn(address indexed owner, address indexed recipient, address indexed token, uint256 value);
+
+  /// @dev get contract address
+  /// @param owner_ Owner of newly created pad
+  /// @param salt_ an arbitrary value
+  function getAddress(address owner_, uint salt_) public view returns (address) {
+    bytes memory bytecode = _getBytecode(owner_);
+
+    bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), salt_, keccak256(bytecode)));
+
+    return address(uint160(uint(hash)));
+  }
+
+
+  /// @dev get byte code of launchpad Contract
+  /// @param owner_ Owner of newly created pad
+  function _getBytecode(address owner_) private pure returns (bytes memory) {
+    bytes memory bytecode = type(BaryonPad).creationCode;
+
+    return abi.encodePacked(bytecode, abi.encode(owner_));
+  }
+
+
+  /// @dev deploy new contract with CREATE2 opcode
+  /// @param code_ code of contract
+  /// @param salt_ an arbitrary value
+  function _deploy(bytes memory code_, uint256 salt_) private returns (address){
+    address addr;
+    assembly {
+      addr := create2(0, add(code_, 0x20), mload(code_), salt_)
+      if iszero(extcodesize(addr)) {
+      revert(0, 0)
+      }
+    }
+    return addr;
+  }
+
+  /// @dev create a new pad with CREATE2 opcode
+  /// @param owner_ Owner of newly created pad
+  /// @param salt_ an arbitrary value
+  function createPad(address owner_, uint256 salt_) external returns (address pad) {
+    bytes memory bytecode = _getBytecode(owner_);
+
+    pad = _deploy(bytecode, salt_);
+
+    emit Created(pad);
+  }
+
+  /// @dev withdraw token from contract
+  /// @param token_ address of the token, use address(0) to withdraw gas token
+  /// @param destination_ recipient address to receive the fund
+  /// @param amount_ amount of fund to withdaw
+  function withdraw(address token_, address destination_, uint256 amount_) external onlyOwner {
+    require(destination_ != address(0), "BaryonPad: Destination is zero address");
+
+    uint256 availableAmount;
+    if(token_ == address(0)) {
+      availableAmount = address(this).balance;
+    } else {
+      availableAmount = IERC20(token_).balanceOf(address(this));
+    }
+
+    require(amount_ <= availableAmount, "BaryonPad: Not enough balance");
+
+    if(token_ == address(0)) {
+      destination_.call{value:amount_}("");
+    } else {
+      IERC20(token_).safeTransfer(destination_, amount_);
+    }
+
+    emit Withdrawn(_msgSender(), destination_, token_, amount_);
+  }
+
+  /// @dev withdraw NFT from contract
+  /// @param token_ address of the token, use address(0) to withdraw gas token
+  /// @param destination_ recipient address to receive the fund
+  /// @param tokenId_ ID of NFT to withdraw
+  function withdrawNft(address token_, address destination_, uint256 tokenId_) external onlyOwner {
+    require(destination_ != address(0), "BaryonPad: destination is zero address");
+
+    IERC721(token_).transferFrom(address(this), destination_, tokenId_);
+
+    emit Withdrawn(_msgSender(), destination_, token_, 1);
+  }
+}
---
+++ evm/contracts/lib/Token.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./Common.sol";
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+
+  /**
+   * @dev Returns the amount of tokens owned by `account`.
+   */
+  function balanceOf(address account) external view returns (uint256);
+
+  /**
+   * @dev Moves `amount` tokens from the caller's account to `recipient`.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Moves `amount` tokens from `sender` to `recipient` using the
+   * allowance mechanism. `amount` is then deducted from the caller's
+   * allowance.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
+
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure (when the token
+ * contract returns false). Tokens that return no value (and instead revert or
+ * throw on failure) are also supported, non-reverting calls are assumed to be
+ * successful.
+ * To use this library you can add a `using SafeERC20 for IERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
+library SafeERC20 {
+
+  using Address for address;
+
+  function safeTransfer(IERC20 token, address to, uint256 value) internal {
+    _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+  }
+
+  function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+    _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+  }
+
+  /**
+    * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+    * on the return value: the return value is optional (but if data is returned, it must not be false).
+    * @param token The token targeted by the call.
+    * @param data The call data (encoded using abi.encode or one of its variants).
+    */
+  function _callOptionalReturn(IERC20 token, bytes memory data) private {
+    // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+    // we're implementing it ourselves. We use {Address.functionCall} to perform this call, which verifies that
+    // the target address contains contract code and also asserts for success in the low-level call.
+
+    bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
+    if (returndata.length > 0) {
+      // Return data is optional
+      require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+    }
+  }
+}
+
+/**
+ * @dev Required interface of an ERC721 compliant contract.
+ */
+interface IERC721 {
+
+  /**
+   * @dev Transfers `tokenId` token from `from` to `to`.
+   *
+   * WARNING: Usage of this method is discouraged, use {safeTransferFrom} whenever possible.
+   *
+   * Requirements:
+   *
+   * - `from` cannot be the zero address.
+   * - `to` cannot be the zero address.
+   * - `tokenId` token must be owned by `from`.
+   * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transferFrom(
+    address from,
+    address to,
+    uint256 tokenId
+  ) external;
+}
